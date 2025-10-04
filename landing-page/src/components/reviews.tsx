4f0a94a6eaf58c99a8a6c3bf28aa5e075821ee62
pawan Sharma <130146893+pawan-pandit@users.noreply.github.com>
2025-10-04 13:43:45 +0530

    change s

--- landing-page/src/components/reviews.tsx
+++ landing-page/src/components/reviews.tsx
@@ -15,7 +15,7 @@ const testimonials: Testimonial[] = [
     title: "Online University",
     achievement: "Tom recently made his first $100K in sales.",
     videoUrl:
-      "ena.mp4",
+      "enaa.mp4",
   },
   {
     id: "2",
